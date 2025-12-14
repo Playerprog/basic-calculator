c4346b4d022559562059b3e5086fb0575a4c8005
Playerprog <samuelcacapava11@gmail.com>
2025-12-14 00:53:21 +0000

    add: loop added to the program
    fix: some errors have been corrected when the program was running
    
    refactor: reoganizing functions and certain loops

--- calculator.py
+++ calculator.py
@@ -1,20 +1,14 @@
 print("Welcome to the Calculator!")
 
-n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number: "))
+# simple menu to choose the operation
 
-valid = ["+", "-", "*", "/"]
+def menu():
+    print("1 - sum")
+    print("2 - subtracion")
+    print("3 - multiplication")
+    print("4 - division")
 
-#only allow valid characters
-
-while True:
-    carac = input("Type + or - or * or /): ")
-    if carac in valid:
-        break
-    else:
-        print("Choose a valid option!")
-
-#functions for basic calculation types
+# functions for basic calculation types
 
 def somar(n1, n2):
     return n1 + n2
@@ -26,23 +20,61 @@ def multiplicar(n1, n2):
     return n1 * n2
 
 def dividir(n1, n2):
-    if n2 == 0:
-        return "Error: It is not possible to divide by zero!"
-    else:
-      return n1 / n2
+    return n1 / n2
 
+
+while True:    
+    
+    menu()
+
+# operation validation
+    
+    while True:
+        
+        valid_operation = ["1", "2", "3", "4"]
+        
+        print("Choose the number that corresponds to your operation:")
+        
+        operation = input()
+        if operation in valid_operation:
+            break
+        print("Invalid operation!")
+
+    while True:
+        try:
+            n1 = float((input("Enter the first number: ")))
+            n2 = float((input("Enter the second number: ")))
+            
+            if operation == "4" and n2 == 0:
+                print("Denominator cannot be 0")
+                continue
+                
+            break
+        except ValueError:
+            print("Enter only numbers, not")
+
+    
 #result of the mathematical operation: 
 
-if carac == "+":
-    print(f"The result is: {somar(n1, n2)}")
+    if operation == "1":
+        print(f"The result is: {somar(n1, n2)}")
 
-elif carac == "-":
-    print(f"The result is: {subtrair(n1, n2)}")
+    elif operation == "2":
+        print(f"The result is: {subtrair(n1, n2)}")
 
-elif carac == "*":
-    print(f"The result is: {multiplicar(n1, n2)}")
+    elif operation == "3":
+        print(f"The result is: {multiplicar(n1, n2)}")
 
-elif carac == "/":
-    print(f"The result is: {dividir(n1, n2)}")
+    elif operation == "4":
+        print(f"The result is: {dividir(n1, n2)}")
+    
+# program end option
+
+    print("Do you want to stop running the program?")
+    finish = input().lower()
+    if finish == "yes":
+        break
+    elif finish != "no":
+        print("Invalid operation!")
 
 
